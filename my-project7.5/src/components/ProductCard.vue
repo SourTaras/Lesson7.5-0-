cf5b1681be95d2290c7ada36bcc2f7873c314741
SourTaras <velgantaras03@gmail.com>
2025-08-11 19:11:13 +0300

    last commit

---
+++ my-project7.5/src/components/ProductCard.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="product-card">
+<img :src="image" :alt="name" class="product-image" />
+<h2>{{ name }}</h2>
+<p>{{ description }}</p>
+<p>{{ price }}</p>
+<button>Купити</button>
+</div>
+</template>
+<script>
+export default{
+    name: "ProductCardComponent",
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
