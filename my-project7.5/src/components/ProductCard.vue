2fe483c41cfdcd098956507ae87145f1557615f8
SourTaras <velgantaras03@gmail.com>
2025-08-14 17:00:18 +0300

    style

--- my-project7.5/src/components/ProductCard.vue
+++ my-project7.5/src/components/ProductCard.vue
@@ -4,7 +4,7 @@
 <h2>{{ name }}</h2>
 <p>{{ description }}</p>
 <p>{{ price }}</p>
-<button>Купити</button>
+<button class="buy-button">Купити</button>
 </div>
 </template>
 <script>
@@ -30,3 +30,35 @@ export default{
     }
 }
 </script>
+<style scoped>
+.product-card{
+    max-width: 300px;
+    border: 1px solid #e0e0e0;
+    border-radius: 12px;
+    box-shadow:  4px 8px rgba(0, 0, 0, 0.1);
+    text-align: center;
+}
+.product-image{
+    width: 100%;
+    height: 200px;
+    object-fit: cover;
+}
+.price{
+    font-size: 18px;
+    font-weight: bold;
+    color: #28a745;
+}
+.buy-button{
+    background-color: #007bff;
+    color: #fff;
+    font-weight: bold;
+    border: none;
+    border-radius: 6px;
+    padding: 10px;
+    cursor: pointer;
+    margin-bottom: 20px;
+
+}
+.buy-button: hover{
+    backgorund-color: #0056b3
+}
