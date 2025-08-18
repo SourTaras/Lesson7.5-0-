0ebc70488c13398282422fdf2f705077306492c6
SourTaras <velgantaras03@gmail.com>
2025-08-18 18:32:56 +0300

    fix

--- my-project7.5/src/App.vue
+++ my-project7.5/src/App.vue
@@ -3,19 +3,19 @@
 <HeaderComponent title="Вітаю у Vue-додатку!" mydata="Додаткова інформація" />
 <MessageComponent title="Привіт, Vue!" message="Я вмію створювати компоненти із пропсами!" />
 <MessageComponent title="Що далі?" message="Тепер я можу передавати різні дані в комопоненти" />
-<ProductCardComponent name="Українська вишиванка" description="Стильна вишиванка з традиційними орнаментами" price=1500 image="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTfzL7Q3MpK7qhw_stK9ZruwOj8KKT-esyJDi0CMJS2RIpT0OjnHmvGALxUHwfWb65UE7H5lFZVElwi3Zy0nAUlZgJlyEvdUK6H6R88GflE" />
+<ProductList/> 
 
 </div>
 </template>
 <script>
 import HeaderComponent from "./components/Header.vue"
 import MessageComponent from "./components/MessageComponent.vue"
-import ProductCardComponent from "./components/ProductCard.vue"
+import ProductList from "./components/ProductList.vue"
 export default{
   components: {
     HeaderComponent,
     MessageComponent,
-    ProductCardComponent
+    ProductList
   }
 }
 </script>
