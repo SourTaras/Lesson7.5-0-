76891f84e6db3cc1fcc450095b1918d1f9672c5c
SourTaras <velgantaras03@gmail.com>
2025-08-21 18:56:22 +0300

    papaparse

--- my-project7.5/src/App.vue
+++ my-project7.5/src/App.vue
@@ -4,7 +4,7 @@
 <MessageComponent title="Привіт, Vue!" message="Я вмію створювати компоненти із пропсами!" />
 <MessageComponent title="Що далі?" message="Тепер я можу передавати різні дані в комопоненти" />
 <ProductList/> 
-
+<ReviewsComponent />
 </div>
 <FooterComponent email="example@mail.com" phone="+38009768342" />
 </template>
@@ -12,12 +12,14 @@
 import HeaderComponent from "./components/Header.vue"
 import MessageComponent from "./components/MessageComponent.vue"
 import ProductList from "./components/ProductList.vue"
+import ReviewsComponent from "./components/ReviewsComponent"
 import FooterComponent from "./components/FooterComponent.vue"
 export default{
   components: {
     HeaderComponent,
     MessageComponent,
     ProductList,
+    ReviewsComponent,
     FooterComponent
   }
 }
