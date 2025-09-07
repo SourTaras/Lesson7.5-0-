801e2b53e2e3faea0b35bd0112fdc87feb0aa10e
SourTaras <velgantaras03@gmail.com>
2025-09-07 17:41:14 +0300

    style

--- my-project7.5/src/components/ManageCart.vue
+++ my-project7.5/src/components/ManageCart.vue
@@ -1,26 +1,60 @@
 <template>
-<div>
-<h2>Кошик покупок</h2>
-<p v-if="cart.length === 0">Кошик порожній</p>
-<ul>
-<li v-for="(item,index) in cartItems" :key="index">
-<img :src="item.image" :alt="item.name" width="50" />
-{{ item.name }} - {{ item.price }} грн
-<button @click="removeItem(index)">Видалити</button>
-</li>
-</ul>
-<p v-if="cart.length > 0">Загальна сума : {{ totalPrice }}</p>
-</div>
+  <div>
+    <h2>Кошик покупок</h2>
+    <p v-if="cartItems && cartItems.length === 0" id="wideCart">Кошик порожній</p>
+    <ul v-if="cartItems && cartItems.length > 0">
+      <li v-for="(item, index) in cartItems" :key="index">
+        <img :src="item.image" :alt="item.name" width="50" />
+        {{ item.name }} - {{ item.price }} грн
+        <button @click="removeItem(index)">Видалити</button>
+      </li>
+    </ul>
+    <p v-if="cartItems && cartItems.length > 0" id="totalPricee">Загальна сума: {{ totalPrice }}</p>
+  </div>
 </template>
+
 <script>
 import { inject } from "vue";
+
 export default {
-    setup() {
-        const cartItems = inject("cartItems")
-        const totalPrice = inject("totalPrice")
-        const removeItem = inject("removeItem")
-        return {cartItems,totalPrice,removeItem}
-    },
+  setup() {
+    const cartItems = inject("cartItems", []);
+    const totalPrice = inject("totalPrice", 0);
+    const removeItem = inject("removeItem", () => {});
+
+    console.log("Injected cartItems:", cartItems); // діагностика
+
+    return { cartItems, totalPrice, removeItem };
+  },
+};
+</script>
+<style scoped>
+ul{
+    list-style: none;
+    text-align: center;
+    margin: 0 auto;
+}
+h2{
+    text-align:center;
+    font-weight: bold;
 
 }
-</script>
+button{
+    color: black;
+    background-color: red;
+    border: 2px solid grey;
+    font-weight: bold;
+}
+#totalPricee{
+    text-align: center;
+    font-weight:bold;
+}
+li{
+    gap: 30vw;
+
+}
+#wideCart{
+    text-align:center;
+    font-weight:bold;
+}
+</style>
